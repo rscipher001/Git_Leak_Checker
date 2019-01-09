4f2dc8e145571ebf2d0f5dd9f7d8194fab56cfab
Ravindra <rscipher001@gmail.com>
2019-01-09 18:00:31 +0530

    Major improvments

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Git_Leak_Checker
 
 ## Description
-`.git` directory can leak complete source code of a website, I wrote this script to find vulnerable domains, Supply it a list of domains and it will write vulnerable domains in another file,
+`.git` directory can leak complete source code of a website, I wrote this script to find vulnerable domains.
+
+## How to use
+You need a list of domains you want to test in textfile without protocol name.
+Ex:
+
+```
+api.google.com
+google.com
+developer.google.com
+...
+education.google.com
+```
+
+Clone it and run
+```bash
+./git_leak_checker.py -i file.txt -o out.txt -t 2
+```
+
+- `-i`: Input file containing domain list
+- `-o`: Output file containing vulnerable domain list
+- `-t`: Curl timeout [Keep low]
 
 ## Journey
 - I wrote it in python and it didn't work the way it should
@@ -26,6 +47,13 @@ pkg i python python-dev openssl openssl-dev curl clang libcrypt libcrypt-dev lib
 export PYCURL_SSL_LIBRARY=openssl
 pip install pycurl
 ```
+
+### Curl TimeOut
+On a vulnerable server it will take less than a second to verify vulnerability so it is a good idea to keep it low but you want to be sure then leave default which is 10.
+
+### Changlog
+- Merged UA in python file now script is portable
+- File that contains `domain` and all `.txt` file are ignored by git
 
 ## Author
 - Ravindra Sisodia
--- git_leak_checker.py
+++ git_leak_checker.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Author: infosecravindra
+
 
 import argparse                        # For CLI arguments
 import certifi                         # For HTTPS support
@@ -11,8 +12,10 @@ from io import BytesIO                 # For buffering curl data
 
 # Argument stuff
 parser = argparse.ArgumentParser(description='Check .git leaks on domains')
-parser.add_argument('input_file', help="Name of file contains domain list")
-parser.add_argument('output_file', help="This file will be written on positive find")
+requiredNamed = parser.add_argument_group('required arguments')
+requiredNamed.add_argument('-i', help="This file contains all domain list", dest='input_file', required=True)
+parser.add_argument('-o', help="This file will contain vulnerable domains", default='vulnerable_domains.txt', dest='output_file')
+parser.add_argument('-t', help="Set curl timeout value, default is 10", default=10, type=int, dest='timeout')
 args = parser.parse_args()
 
 
@@ -20,23 +23,17 @@ args = parser.parse_args()
 user_agents = set()             # Empty UA set
 input_file = args.input_file
 output_file = args.output_file
-num_lines = sum(1 for line in open(input_file))
-total = 0
+timeout = args.timeout
 success = 0
+total = 0
+num_lines = sum(1 for line in open(input_file))
+
 
 def read_ua():                  # Read all UA
-  try:
-    with open('user-agents.txt') as user_agents_file:
-      for user_agent in user_agents_file:
-        user_agents.add(user_agent.strip())
-
-  except KeyboardInterrupt:
-    print ("Ctrl-c pressed ...")
-    sys.exit(1)
-
-  except Exception as e:
-    print('Error: %s' % e)
-    sys.exit(1)
+  user_agents.add("Mozilla/5.0 (X11; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0")
+  user_agents.add("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0")
+  user_agents.add("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
+  user_agents.add("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
 
 
 def random_ua():                # Get Random UA
@@ -52,7 +49,7 @@ def check_domain(final_url):    # Network call
     c.setopt(c.WRITEDATA, buffer)
     c.setopt(pycurl.ACCEPT_ENCODING, 'identity')
     c.setopt(pycurl.FOLLOWLOCATION, 1)
-    c.setopt(pycurl.TIMEOUT, 10)
+    c.setopt(pycurl.TIMEOUT, timeout)             # Refer Global Variable
     c.setopt(pycurl.URL, final_url)
     c.setopt(pycurl.USERAGENT, random_ua())
     c.perform()
@@ -91,13 +88,16 @@ def write_domain(url):          # Write vulnerables domains to output file
 
 if __name__ == "__main__":      # Main
   read_ua()
+
   try:
     with open(input_file) as urls:
       for raw_url in urls:
         if raw_url != "":
           url = raw_url.strip()
           total = total + 1
-          print("Progress:", total, "/", num_lines, ", Success:", success, "\r", end="")
+
+          print("\rProgress: {}/{} | Success: {}".format(total, num_lines, success), end = "")
+
           final_url = url + "/.git/HEAD"
           body = check_domain(final_url)
 

c4c54759731e39f4b11c10f386e5406ca96f7268
Ravindra <rscipher001@gmail.com>
2019-01-24 17:49:24 +0530

    Unstable Commit

--- git_leak_checker.py
+++ git_leak_checker.py
@@ -8,25 +8,25 @@ import pycurl                          # For making network requests
 import random                          # For choosing random UA
 import sys                             # For unexpected exit
 from io import BytesIO                 # For buffering curl data
+from multiprocessing import Pool       # For parallel processing
 
 
 # Argument stuff
-parser = argparse.ArgumentParser(description='Check .git leaks on domains')
-requiredNamed = parser.add_argument_group('required arguments')
-requiredNamed.add_argument('-i', help="This file contains all domain list", dest='input_file', required=True)
-parser.add_argument('-o', help="This file will contain vulnerable domains", default='vulnerable_domains.txt', dest='output_file')
-parser.add_argument('-t', help="Set curl timeout value, default is 10", default=10, type=int, dest='timeout')
-args = parser.parse_args()
-
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--inputfile', default='input.txt', help='input file')
+ap.add_argument('-o', '--outputfile', default='output.txt', help='output file')
+ap.add_argument('-t', '--threads', default=200, help='threads')
+args = ap.parse_args()
 
 # Global variables
 user_agents = set()             # Empty UA set
-input_file = args.input_file
-output_file = args.output_file
-timeout = args.timeout
+inputfile = args.inputfile
+outputfile = args.outputfile
+max_process = 50
 success = 0
+timeout = 5
 total = 0
-num_lines = sum(1 for line in open(input_file))
+num_lines = sum(1 for line in open(inputfile))
 
 
 def read_ua():                  # Read all UA
@@ -40,40 +40,46 @@ def random_ua():                # Get Random UA
   return random.sample((user_agents),1)[0]
 
 
-def check_domain(final_url):    # Network call
-  try:
-    buffer = BytesIO()
+def check_domain(raw_url):    # Network call
+  final_url = raw_url.strip()
+  if final_url != "":
+    final_url = final_url + "/.git/HEAD"
 
-    c = pycurl.Curl()    
-    c.setopt(c.CAINFO, certifi.where())
-    c.setopt(c.WRITEDATA, buffer)
-    c.setopt(pycurl.ACCEPT_ENCODING, 'identity')
-    c.setopt(pycurl.FOLLOWLOCATION, 1)
-    c.setopt(pycurl.TIMEOUT, timeout)             # Refer Global Variable
-    c.setopt(pycurl.URL, final_url)
-    c.setopt(pycurl.USERAGENT, random_ua())
-    c.perform()
-    c.close()
+    try:
+      buffer = BytesIO()
 
-    body = buffer.getvalue().decode('iso-8859-1')
-    buffer.close()
-    return body
+      c = pycurl.Curl()    
+      c.setopt(c.CAINFO, certifi.where())
+      c.setopt(c.WRITEDATA, buffer)
+      c.setopt(pycurl.ACCEPT_ENCODING, 'identity')
+      c.setopt(pycurl.FOLLOWLOCATION, 1)
+      c.setopt(pycurl.TIMEOUT, timeout)             # Refer Global Variable
+      c.setopt(pycurl.URL, final_url)
+      c.setopt(pycurl.USERAGENT, random_ua())
+      c.perform()
+      c.close()
 
-  except pycurl.error:
-    return ""
+      body = buffer.getvalue().decode('iso-8859-1')
+      buffer.close()
 
-  except KeyboardInterrupt:
-    print ("Ctrl-c pressed ...")
-    sys.exit(1)
+      if "ref: refs" in body:
+        write_domain(final_url)
 
-  except Exception as e:
-    print('Error: %s' % e)
-    sys.exit(1)
+    except pycurl.error:
+      return ""
+
+    except KeyboardInterrupt:
+      print ("Ctrl-c pressed ...")
+      sys.exit(1)
+
+    except Exception as e:
+      print('Error: %s' % e)
+      sys.exit(1)
 
 
 def write_domain(url):          # Write vulnerables domains to output file
   try:
-    fout =  open(output_file, 'a')
+    fout =  open(outputfile, 'a')
     fout.write("\n" + url)
     fout.close()
 
@@ -88,27 +94,14 @@ def write_domain(url):          # Write vulnerables domains to output file
 
 if __name__ == "__main__":      # Main
   read_ua()
-
   try:
-    with open(input_file) as urls:
-      for raw_url in urls:
-        if raw_url != "":
-          url = raw_url.strip()
-          total = total + 1
+    domains = open(inputfile, "r").readlines()
 
-          print("\rProgress: {}/{} | Success: {}".format(total, num_lines, success), end = "")
+  except FileNotFoundError as e:
+    print(e)
+    exit(e.errno)
 
-          final_url = url + "/.git/HEAD"
-          body = check_domain(final_url)
-
-          if "ref: refs" in body:
-            write_domain(url)
-            success = success + 1
-
-  except KeyboardInterrupt:
-    print ("Ctrl-c pressed ...")
-    sys.exit(1)
-
-  except Exception as e:
-    print('Error: %s' % e)
-    sys.exit(1)
+  print("Scanning...")
+  pool = Pool(processes = max_process)
+  pool.map(check_domain, domains)
+  print("Finished")
